d446fed4eecf685ed2dfb594808187e0fc15c631
Michelangelo FIORE <mfiore@maxc1.(none)>
2015-05-19 16:50:16 +0200

    fixed pick&place

--- runSup.sh
+++ runSup.sh
@@ -4,4 +4,4 @@ h2 clean OPRS_SUPR
 
 export OPRS_DATA_PATH=${OPRS_DATA_PATH}:"."
 
-xoprs OPRS_SUP -x ./supervisor/mainPr2.inc  -x ${OPRSGENOM} #-x ${OPRS_ACAPELA_PATH}
+xoprs OPRS_SUP -x ./supervisor/mainPr2.inc  -x ${OPRS_GENOM_PATH} -x ${OPRS_ACAPELA_PATH}
